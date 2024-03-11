2da725095db96be93c4f58116a3013efaf28a295
Emerson Ferreira <emerson.ferreira.net@gmail.com>
2024-03-11 06:34:11 +0000

    Creating a test for dim_customers

---
+++ tests/assert_dim_customers_compare_all_columns.sql
@@ -0,0 +1,17 @@
+with
+    base_fct_orders as (
+        {{
+            audit_helper.compare_all_columns(
+                a_relation=ref("dim_customers"),
+                b_relation=api.Relation.create(
+                    database=env_var('DBT_DB'), schema=env_var('DBT_CLONE_SCHEMA', 'dev_clone'), identifier="clone_dim_customers"
+                ),
+                exclude_columns=["_audit_run_id", "run_started_est"],
+                primary_key="customer_id",
+            )
+        }}
+        where conflicting_values > 0
+    )
+
+select *
+from base_fct_orders
