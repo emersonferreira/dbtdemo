471467f1715765e7ac0567a3729eef6cb8f72fa5
Emerson Ferreira <emerson.ferreira.net@gmail.com>
2024-03-12 03:26:45 +0000

    Adding the dbt_profiler package

--- models/clone/clone_fct_orders.sql
+++ models/clone/clone_fct_orders.sql
@@ -8,4 +8,4 @@ select
     *,
     '{{ env_var("DBT_CLOUD_RUN_ID", "manual") }}' as _audit_run_id,
     '{{ run_started_at.astimezone(modules.pytz.timezone("Australia/Sydney")) }}' as run_started_est
-from {{ ref('fct_orders') }}
+from {{ target.schema }}_marts.fct_orders
