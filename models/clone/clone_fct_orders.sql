f1f3419edf36a5addbfc339b2db43de861c5fa4b
Emerson Ferreira <emerson.ferreira.net@gmail.com>
2024-03-13 03:22:21 +0000

    Rolling back to ref fct_orders

--- models/clone/clone_fct_orders.sql
+++ models/clone/clone_fct_orders.sql
@@ -1,11 +1,12 @@
 {{
     config(
-        materialized='table'
+        materialized='table',
+        tags=['clone_model']
     )
 }}
 
 select 
     *,
-    '{{ env_var("DBT_CLOUD_RUN_ID", "manual") }}' as _audit_run_id,
+    '{{ env_var("DBT_CLOUD_RUN_ID", "manual") }}' as audit_run_id,
     '{{ run_started_at.astimezone(modules.pytz.timezone("Australia/Sydney")) }}' as run_started_est
-from {{ target.schema }}_marts.fct_orders
+from {{ ref('fct_orders') }}
