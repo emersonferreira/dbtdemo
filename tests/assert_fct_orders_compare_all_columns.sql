ad905597d1ce22c78f4f838e6de71394d0dac0d0
Emerson Ferreira <emerson.ferreira.net@gmail.com>
2024-03-15 05:15:46 +0000

    Adding a generic test and playing with macros

--- tests/assert_fct_orders_compare_all_columns.sql
+++ tests/assert_fct_orders_compare_all_columns.sql
@@ -1,9 +1,12 @@
 with
+    base_clone_fct_orders as (
+        select * from {{ ref('clone_fct_orders') }}
+    ),
     base_fct_orders as (
         {{
             audit_helper.compare_all_columns(
                 a_relation=ref("fct_orders"),
-                b_relation=ref("clone_fct_orders"),
+                b_relation=ref("ephemeral_clone_fct_orders"),
                 exclude_columns=["updated_at","audit_run_id", "run_started_est"],
                 primary_key="order_id",
             )
