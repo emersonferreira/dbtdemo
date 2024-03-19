ea66283cbb6c0a3418fc39dfdf63fadad7c6f792
Emerson Ferreira <emerson.ferreira.net@gmail.com>
2024-03-19 05:11:24 +0000

    Creating the clone.

---
+++ models/clone/clone_fct_orders.sql
@@ -0,0 +1,12 @@
+{{
+    config(
+        materialized='table',
+        tags=['clone_model']
+    )
+}}
+
+select 
+    *,
+    '{{ env_var("DBT_CLOUD_RUN_ID", "manual") }}' as audit_run_id,
+    '{{ run_started_at.astimezone(modules.pytz.timezone("Australia/Sydney")) }}' as run_started_est
+from {{ ref("fct_orders") }}
